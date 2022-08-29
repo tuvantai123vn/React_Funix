d340d8208f8e0476f204ce3d1173ff6a7a52b4d4
tuvantai123vn <nguyenthanhtu.cdct@gmail.com>
2022-08-29 08:02:19 +0700

    update2

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, Row, FormFeedback } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = (val) => val && val.length;
+const maxLenght = (len) => (val) => !(val) || (val.length <= len);
+const minLenght = (len) => (val) => (val) && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9_%+-] + @[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
     constructor(props) {
@@ -69,7 +74,15 @@ class Contact extends Component {
                                 <Col md={10}>
                                     <Control.text model='.firstname' id="firstname" name="firstname"
                                         placeholder="First Name"
-                                        className='form-control' />
+                                        className='form-control' validators={{
+                                            required, minLenght: minLenght(3), maxLenght: maxLenght(15)
+                                        }} />
+                                    <Errors className='text-danger' model='.firstname' show='touched'
+                                        messages={{
+                                            required: 'Required',
+                                            minLenght: 'Must be greater than 2 characters',
+                                            maxLenght: 'Must be 15 character or less'
+                                        }} />
                                 </Col>
                             </Row>
                             <Row className='form-group'>
@@ -77,7 +90,15 @@ class Contact extends Component {
                                 <Col md={10}>
                                     <Control.text model='.lastname' id="lastname" name="lastname"
                                         placeholder="Last Name"
-                                        className='form-control' />
+                                        className='form-control' validators={{
+                                            required, minLenght: minLenght(3), maxLenght: maxLenght(15)
+                                        }} />
+                                    <Errors className='text-danger' model='.lastname' show='touched'
+                                        messages={{
+                                            required: 'Required',
+                                            minLenght: 'Must be greater than 2 characters',
+                                            maxLenght: 'Must be 15 character or less'
+                                        }} />
                                 </Col>
                             </Row>
                             <Row className='form-group'>
@@ -85,15 +106,31 @@ class Contact extends Component {
                                 <Col md={10}>
                                     <Control.text model='.telnum' id="telnum" name="telnum"
                                         placeholder="Tel. Number"
-                                        className='form-control' />
+                                        className='form-control' validators={{
+                                            required, minLenght: minLenght(3), maxLenght: maxLenght(15), isNumber
+                                        }} />
+                                    <Errors className='text-danger' model='.telnum' show='touched'
+                                        messages={{
+                                            required: 'Required',
+                                            minLenght: 'Must be greater than 2 number',
+                                            maxLenght: 'Must be 15 number or less', isNumber: 'Must be a number'
+                                        }} />
                                 </Col>
                             </Row>
                             <Row className='form-group'>
                                 <Label htmlFor="email" md={2}>Email</Label>
                                 <Col md={10}>
-                                    <Control.text model="email" id="email" name="email"
+                                    <Control.text model=".email" id="email" name="email"
                                         placeholder="Email"
-                                        className='form-control' />
+                                        className='form-control'
+                                        validators={{
+                                            required, validEmail
+                                        }} />
+                                    <Errors className='text-danger' model='.email' show='touched'
+                                        messages={{
+                                            required: 'Required',
+                                            validEmail: 'invalid Email Address'
+                                        }} />
                                 </Col>
                             </Row>
                             <Row className='form-group'>
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import React, { useState } from "react";
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Col, Row, Label, Button } from "reactstrap";
 import { Link } from "react-router-dom"
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
 
 function RenderDish({ dish }) {
     return (
@@ -15,8 +17,19 @@ function RenderDish({ dish }) {
         </div>
     );
 }
-
+function handleSubmit(value) {
+    console.log('Current State is: ' + JSON.stringify(value));
+    alert("Current State is: " + JSON.stringify(value));
+}
 function RenderComments({ comments }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const required = (val) => val && val.length;
+    const maxLenght = (len) => (val) => !(val) || (val.length <= len);
+    const minLenght = (len) => (val) => (val) && (val.length >= len);
+    const isNumber = (val) => !isNaN(Number(val));
+    const validEmail = (val) => /^[A-Z0-9_%+-] + @[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
     if (comments != null)
         return (
             <div className="col-12 col-md-5 m-1">
@@ -31,6 +44,60 @@ function RenderComments({ comments }) {
                             </li>
                         )
                     })}
+                    <Button outline onClick={toggle}>
+                        <span className='fa fa-pencil'></span> Submit Comments
+                    </Button>
+                    <Modal isOpen={isOpen} toggle={toggle}>
+                        <ModalHeader toggle={toggle}>
+                            Submit Comment
+                        </ModalHeader>
+                        <ModalBody>
+                            <LocalForm onSubmit={(value) => this.handleSubmit(value)}>
+                                <Row className='form-group'>
+                                    <Label htmlFor="rating" md={2}>Rating</Label>
+                                    <Col md={12}>
+                                        <Control.select model=".rating" name="rating" className='form-control'>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </Control.select>
+                                    </Col>
+                                </Row>
+                                <Row className='form-group'>
+                                    <Label htmlFor="yourname" md={12}>First Name</Label>
+                                    <Col md={12}>
+                                        <Control.text model='.yourname' id="yourname" name="yourname"
+                                            placeholder="Your Name"
+                                            className='form-control' validators={{
+                                                required, minLenght: minLenght(3), maxLenght: maxLenght(15)
+                                            }} />
+                                        <Errors className='text-danger' model='.yourname' show='touched'
+                                            messages={{
+                                                required: 'Required',
+                                                minLenght: 'Must be greater than 2 characters',
+                                                maxLenght: 'Must be 15 character or less'
+                                            }} />
+                                    </Col>
+                                </Row>
+                                <Row className='form-group'>
+                                    <Label htmlFor="message" md={12}>Your Feedback</Label>
+                                    <Col md={12}>
+                                        <Control.textarea model='.message' id="message" name="message"
+                                            rows="12" className='form-control'></Control.textarea>
+                                    </Col>
+                                </Row>
+                                <Row className='form-group'>
+                                    <Col md={9}>
+                                        <Button type="submit" color="primary">
+                                            Send Feedback
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </LocalForm>
+                        </ModalBody>
+                    </Modal>
                 </ul>
             </div>
         );
@@ -39,34 +106,32 @@ function RenderComments({ comments }) {
 }
 
 const DishDetail = (props) => {
-    if (props.dish != null)
-        return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem>
-                            <Link to='/home'>Home</Link>
-                        </BreadcrumbItem>
-                        <BreadcrumbItem>
-                            <Link to='/menu'>Menu</Link>
-                        </BreadcrumbItem>
-                        <BreadcrumbItem active>
-                            {props.dish.name}
-                        </BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
+
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to='/home'>Home</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <Link to='/menu'>Menu</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        {props.dish.name}
+                    </BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
                 </div>
             </div>
-        )
-    else
-        return <div></div>
+            <div className="row">
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.comments} />
+            </div>
+        </div>
+    )
 }
 
 export default DishDetail;
