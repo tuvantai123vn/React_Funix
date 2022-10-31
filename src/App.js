07235091f49ce7113da62e6b3021d19395d129c7
tuvantai123vn <nguyenthanhtu.cdct@gmail.com>
2022-10-31 19:29:56 +0700

    Lab03.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './Component/MenuComponent';
+import {DISHES} from './shared/dishes'
 
-function App() {
+class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    dishes: DISHES
+  };
+}
+render(){
   return (
     <div className="App">
         <Navbar dark color="primary">
@@ -10,9 +19,9 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes} />
       </div>
   );
-}
+}}
 
 export default App;
